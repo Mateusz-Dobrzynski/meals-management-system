37034f74b94d5345d5792369803fe855b1340522
Hubert Borysowski <borysowski.git@gmail.com>
2023-08-31 22:21:23 +0200

    [38] Basic Email Service ✉️ Emails are now sent.

--- src/main/java/org/lookout_studios/meals_management_system/meals_management_system/EmailController.java
+++ src/main/java/org/lookout_studios/meals_management_system/meals_management_system/EmailController.java
@@ -3,19 +3,18 @@ package org.lookout_studios.meals_management_system.meals_management_system;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class EmailController {
     
     private EmailService mailService;
-
+    
     public EmailController(EmailService mailService) {
         this.mailService = mailService;
     }
-
     @GetMapping("/sendemail")
     public String sendEmail() {
-        mailService.sendSimpleEmail("Odbiorca <odbiorca@maila.pl>", "Test e-mail", "Testing email functionality");
-
+        mailService.sendSimpleEmail("yourmoms@mail.com", "Test e-mail", "Testing email functionality");
         return "E-mail sent!";
     }
 }
--- src/main/java/org/lookout_studios/meals_management_system/meals_management_system/EmailService.java
+++ src/main/java/org/lookout_studios/meals_management_system/meals_management_system/EmailService.java
@@ -1,12 +1,16 @@
 package org.lookout_studios.meals_management_system.meals_management_system;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailService {
     
+    @Autowired
     private JavaMailSender javaMailSender;
-
+    
     public EmailService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
@@ -14,11 +18,10 @@ public class EmailService {
     public void sendSimpleEmail(String to, String subject, String content) {
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(to);
-        msg.setFrom("Example <from@email.com>");
+        msg.setFrom("from@example.com");
         
         msg.setSubject(subject);
         msg.setText(content);
-
         javaMailSender.send(msg);
     }
 }
